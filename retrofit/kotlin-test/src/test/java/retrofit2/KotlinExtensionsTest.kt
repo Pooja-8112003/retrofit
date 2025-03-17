f2eefbf3cae096660cf27319f4d8a53ba43506c2
Pooja-8112003 <pooja.m.intern@lightmetrics.co>
2025-03-17 13:05:39 +0530

    modified the kotlin file

--- retrofit/kotlin-test/src/test/java/retrofit2/KotlinExtensionsTest.kt
+++ retrofit/kotlin-test/src/test/java/retrofit2/KotlinExtensionsTest.kt
@@ -42,4 +42,17 @@ fun main(args: Array<String>) {
 
     println("The sum is: $sum")
 }
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+
+    print("Enter a number: ")
+    val num = reader.nextInt()
+
+    if (num % 2 == 0)
+        println("$num is even")
+    else
+        println("$num is odd")
+}
+
 }
