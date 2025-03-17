ce6b838f65fa69970e2c15dffa78b0be6ef5bd75
Pooja-8112003 <pooja.m.intern@lightmetrics.co>
2025-03-17 15:03:38 +0530

    final changes 5

--- retrofit/kotlin-test/src/test/java/retrofit2/KotlinSuspendTest.kt
+++ retrofit/kotlin-test/src/test/java/retrofit2/KotlinSuspendTest.kt
@@ -394,8 +394,9 @@ class KotlinSuspendTest {
     override fun isDispatchNeeded(context: CoroutineContext): Boolean = false
     override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
   }
-   
-  fun main(args: Array<String>) {
-    println("Hello, World!")
+  
+
+  fun buz(m: String) {
+   println("another message: $m")
 }
 }
