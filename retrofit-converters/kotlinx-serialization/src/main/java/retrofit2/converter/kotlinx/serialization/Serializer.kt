af30ea0da0c1db51de600fe30e43f319ceb13801
Pooja-8112003 <pooja.m.intern@lightmetrics.co>
2025-03-21 15:42:02 +0530

    Revert "test violation"
    This reverts commit 23c85db8138ab1453432efa2747108e9c70d0962.

--- retrofit-converters/kotlinx-serialization/src/main/java/retrofit2/converter/kotlinx/serialization/Serializer.kt
+++ retrofit-converters/kotlinx-serialization/src/main/java/retrofit2/converter/kotlinx/serialization/Serializer.kt
@@ -40,7 +40,6 @@ internal sealed class Serializer {
 
     override fun <T> toRequestBody(contentType: MediaType, saver: SerializationStrategy<T>, value: T): RequestBody {
       val bytes = format.encodeToByteArray(saver, value)
-	do{}while(true)
       return RequestBody.create(contentType, bytes)
     }
   }
